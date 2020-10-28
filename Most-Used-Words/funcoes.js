32a4177b44b5a7f0a0ee9632fb48acbd51359ae0
eldolopes <eldolopes@gmail.com>
2020-10-28 20:51:43 -0300

    commit

--- Most-Used-Words/funcoes.js
+++ Most-Used-Words/funcoes.js
@@ -1,19 +1,42 @@
-const { error } = require('console')
 const fs = require('fs')
 const path = require('path')
 
-const readDirectory = () => {    
-    const way = path.join(__dirname, 'subtitles')   
-    let files = fs.readdirSync(way)
+const readDirectory = (way) => {
     return new Promise((resolve, reject) => {
         try {
-            resolve(files.map(file => path.join(way, file)))            
+            const files = fs.readdirSync(way).map(file => path.join(way, file))
+            resolve(files)
         } catch (error) {
             reject(`Erro no processo: ${error}`)
         }
-    })  
+    })
+}
+
+const elementsEndWith = (array, padrao) => {
+    return array.filter(el => el.endsWith(padrao))
+}
+
+const replaceValue = (array, value, newvalue) => {
+    return array.map(el => el.split(value).join(newvalue))
+}
+
+const readFile = (way) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const data = fs.readFileSync(way, { encoding: 'utf-8' })
+            resolve(data.toString())
+        } catch(err) {
+            reject(`Erro ao ler arquivo: ${err}`)
+        }
+    })
+}
+const readFiles = (ways) => {
+    return Promise.all(ways.map(data => readFile(data)))
 }
 
 module.exports = {
-    readDirectory
+    readDirectory,
+    elementsEndWith,
+    replaceValue,
+    readFiles
 } 
--- Most-Used-Words/index.js
+++ Most-Used-Words/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()  
-const fn = require('./funcoes') 
+const fn = require('./funcoes')
 
-fn.readDirectory()
+
+const path = require('path')
+const way = path.join(__dirname, 'subtitles')
+
+fn.readDirectory(way)
+    .then(data => fn.elementsEndWith(data, '.srt'))
+    .then(data => fn.replaceValue(data, '\\', '/'))
+    .then(data => fn.readFiles(data))
     .then(console.log)
     .catch(console.log)
 
